197b11f89eebd79a1280ebf7adfb7a3cd095a1cd
Ignacio Martinez Soriano <imsoriano67@gmail.com>
2017-12-13 00:29:22 +0100

    Add files via upload

---
+++ DatosR/Trabajando_datos1/ejemplo.R
@@ -0,0 +1,10 @@
+# Simple script de ejemplo
+
+x <- rnorm(10)
+mux = mean(x)
+cat("La media y la desviacion estandar de x es  ",mean(x), sd(x),"\n")
+
+# Resumen de resultados
+summary(x)
+cat("El resumen estadistico es \n",summary(x),"\n")
+print(summary(x))
